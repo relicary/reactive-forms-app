a0fc3bd8b329f7fb218443ba5232789cbe0c5bd7
Juan Cremades <relicary@gmail.com>
2025-03-12 17:20:09 +0100

    ROUTES: Added APP Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.routes').then( (modules) => modules.reactiveRoutes),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./country/country.routes').then( (modules) => modules.countryRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+];
