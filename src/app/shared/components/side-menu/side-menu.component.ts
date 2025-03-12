720d176eef2ba7db25e0f36552a4dbbeaa54cceb
Juan Cremades <relicary@gmail.com>
2025-03-12 17:43:02 +0100

    COMPONENT: SideMenu filled

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+  reactiveMenu: MenuItem[] = reactiveItems
+    .filter((item) => item.path !== '**')
+    .map((item) => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`,
+    }));
+
+  authMenu: MenuItem[] = [
+    {
+      title: 'Sign up',
+      route: './auth',
+    },
+  ];
+
+  countryMenu: MenuItem[] = [
+    {
+      title: 'Countries',
+      route: './country',
+    },
+  ];
+}
