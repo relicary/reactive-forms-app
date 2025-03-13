e877a0371e8d832279abe832ce7e0f54ccda099f
Juan Cremades <relicary@gmail.com>
2025-03-13 13:07:59 +0100

    FORMS: Bonding form HTML eoth TS

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,33 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPageComponent { }
+export class DynamicPageComponent {
+  private fb = inject(FormBuilder);
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favouriteGames: this.fb.array(
+      [
+        ['Metal Gear', Validators.required],
+        ['Mario Kart', Validators.required],
+      ],
+      Validators.minLength(3)
+    ),
+  });
+
+  get favouriteGames() {
+    return this.myForm.get('favouriteGames') as FormArray;
+  }
+}
