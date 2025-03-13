59d4a3af158b9987f98c9277da2d725b8f84195d
Juan Cremades <relicary@gmail.com>
2025-03-13 10:57:05 +0100

    FORMS: Added Sync Validators

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -12,19 +12,29 @@ export class BasicPageComponent {
   private formBuilder = inject(FormBuilder);
 
   myForm = this.formBuilder.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: [
+      '',
+      /* Sync Validators*/
+      [
+        Validators.required,
+        Validators.minLength(3)
+      ],
+      /* Async Validators */ [],
+    ],
+    price: [
+      0,
+      [
+        Validators.required,
+        Validators.min(10)
+      ]
+    ],
+    inStorage: [
+      0,
+      [
+        Validators.required,
+        Validators.min(0)
+      ]
+    ],
   });
-
-  /*
-  myForm = new FormGroup(
-    {
-      name: new FormControl(''),
-      price: new FormControl(0),
-      inStorage: new FormControl(0),
-    }
-  );
-  */
 
 }
